0908f57b260d0b67dba819e280759507bc9ba124
rishabhsingh <rishabhsingh4000@gmail.com>
2024-08-21 03:24:52 +0530

    search_product_items

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('add-product/', add_product, name='add_product'),
     path('view-product/',view_product,name = 'view_product'),
-    path('edit-product/<int:pk>/',edit_product,name = 'edit_product')
+    path('edit-product/<int:pk>/',edit_product,name = 'edit_product'),
+    path('search/', product_search, name='product_search')
 ]
